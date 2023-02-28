53ce07830c7c246b832417919a49059e2180ba70
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-02-28 17:58:05 -0300

    novo lato

--- src/Pergunta.js
+++ src/Pergunta.js
@@ -2,8 +2,19 @@ export default function Pergunta(props) {
 
     return (
         <>
-             <div className="perguntas">
-                <h1>Pergunta 1</h1>
+            <div className="perguntas">
+                <div className="caixa">
+                    <h1>Pergunta 1</h1>
+                </div>
+                <div className="caixa">
+                    <h1>Pergunta 1</h1>
+                </div>
+                <div className="caixa">
+                    <h1>Pergunta 1 </h1>
+                </div>
+                <div className="caixa">
+                    <h1>Pergunta 1</h1>
+                </div>
             </div>
         </>
     );
