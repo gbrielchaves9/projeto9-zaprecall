93f9a2a1fa7b7f2519b71ba6bfab14902eebe074
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-03-06 12:08:54 -0300

    icones indo

--- src/Pergunta.js
+++ src/Pergunta.js
@@ -2,12 +2,17 @@ import seta_play from "./assets/seta_play.png"
 import seta_virar from "./assets/seta_virar.png"
 import styled from 'styled-components';
 import { useState } from "react";
+import icone_certo from "./assets/icone_certo.png"
+import icone_erro from "./assets/icone_erro.png"
+import icone_quase from "./assets/icone_quase.png"
 
 
 export default function Pergunta({ i, ques }) {
   const [numero, setNumero] = useState(false)
   const [exibePergunta, setExibePergunta] = useState(false)
   const [respondi, setRespondi] = useState(false)
+  const [addLinha, setAddLinha] = useState(false)
+
 
   function virar(){
     if(!respondi){
@@ -19,35 +24,47 @@ export default function Pergunta({ i, ques }) {
     setExibePergunta(true)
 
   }
-  function voltaNormal (){
+  function voltaNormal (icone){
     setNumero(false)
     setRespondi(true)
+    setAddLinha(icone)
   }
-
+function tipoIcone(){
+  switch(addLinha){
+  case"errou":
+  return icone_erro
+  case"certo":
+  return icone_certo
+  case"quase":
+  return icone_quase
+  default :
+  return seta_play
+  }
+}
   return (
     <>
       {!numero ? (
         <Caixa>
           <h1>Pergunta {i + 1}</h1>
-          <img src={seta_play} alt="forca" data-test="game-image" onClick={virar} />
+          <img src={tipoIcone()} alt="forca" data-test="game-image" onClick={virar} />
         </Caixa>) : (
         <PerguntaEscondida>
           {!exibePergunta ? (
             <>
              <h1>{ques.question}</h1>
-             <img src={seta_virar} onClick={resposta} />
+             <img src={seta_virar} alt="Seta para iniciar pergunta" onClick={resposta} />
              </>
           ):(
             <>
              <ExibiResposta>
         <h1>{ques.answer}</h1>
-        <button onClick={voltaNormal}>
+        <button onClick={()=>voltaNormal("errou")}>
           Não lembrei
         </button>
-        <button onClick={voltaNormal}>
+        <button onClick={()=>voltaNormal("quase")}>
           Quase Não lembrei
         </button>
-        <button onClick={voltaNormal}>
+        <button onClick={()=>voltaNormal("certo")}>
           Zap!
         </button>
       </ExibiResposta>
@@ -57,16 +74,7 @@ export default function Pergunta({ i, ques }) {
     </>
   );
 }
-/* <Caixa>
- <h1>Pergunta {i + 1}</h1>
- <img src={seta_play} alt="forca" data-test="game-image" onClick={ChamaPergunta} />
-</Caixa>*/
 
-/*<PerguntaEscondida>
-<h1>{ques.question}</h1>
-<img src={seta_virar}  onClick={ChamaPergunta} />
-
-</PerguntaEscondida>*/
 
 
 const Caixa = styled.div`
@@ -101,32 +109,32 @@ h1{
 }
 `;
 
-
 const ExibiResposta = styled.div`
-  margin-top: 30px ;
-      width: 100%;
-    height: auto;
-border-radius: 5px;
-padding: 25px;
-background: #FFFFD4;
-display: flex; 
-justify-content:space-between;
-font-family: Recursive;
-h1{
+   margin-top: 30px ;
+  width: 100%;
+  height: auto;
+  border-radius: 5px;
+  padding: 25px;
+  background: #FFFFD4;
+  display: flex; 
+  flex-direction: column;  
+  justify-content: space-between;
+  font-family: Recursive;
+  h1{
     font-size: 18px;
     font-weight: 400;
     font-family: Recursive;
-};
-button{
-  width:90px;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  border-radius:5px;
-  border :1px solid blue;
-  padding:5px;
-}
-
+  };
+  button{
+    width: 90px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    border: 1px solid blue;
+    padding: 5px;
+    margin-top: 10px;  
+  }
 `;
 
 
